141bd0a1f7ad21c10b1f78b47b2f133ba77a8141
MattiLope <126561399+MattiLope@users.noreply.github.com>
2025-12-12 15:00:59 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Car car1 = new Car("Toyota", "Corolla");
+        car1.printData();
+    }
+}
+// Car class
+class Car {
+    public String brand;
+    public String model;
+
+    // Default constructor
+    public Car() {
+        brand = "";
+        model = "";
+    }
+    // Constructor with all parameters
+    public Car(String b, String m) {
+        brand = b;
+        model = m;
+    }
+    // Function to print data from object
+    public void printData() {
+        System.out.println("Brand is: " + brand);
+        System.out.println("Model is: " + model);
+    }
+}
+
