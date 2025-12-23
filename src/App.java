57e575037dfa77b19b84ccc367a8064fc27e1daa
Tia Häggblom <tia.haggblom@student.hamk.fi>
2025-12-23 11:12:10 +0200

    Added Model Year

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Car car1 = new Car("Toyota", "Corolla");
+        Car car1 = new Car("Toyota", "Corolla", 2007);
         car1.printData();
     }
 }
@@ -8,25 +8,30 @@ public class App {
 class Car {
     public String brand;
     public String model;
+    public int modelYear;
 
     // Default constructor
     public Car() {
         brand = "Toyota";
         model = "Corolla";
+        modelYear = 2007;
     }
     // Constructor with all parameters
-    public Car(String b, String m) {
+    public Car(String b, String m, int y) {
         brand = b;
         model = m;
+        modelYear = y;
     }
     // Function to print data from object
     public void printData() {
         System.out.println("Brand is: " + brand);
         System.out.println("Model is: " + model);
+        System.out.println("Model year is: " + modelYear);
+
     }
     // Additional helper method added by Karina     
     public void printInfo() {
-        System.out.println("Car: " + brand + " " + model);
+        System.out.println("Car: " + brand + " " + model + " " + modelYear);
     }
 
 }
